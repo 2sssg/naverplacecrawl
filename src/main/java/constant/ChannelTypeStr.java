a4668a3ec314326ca1f4d55576390fa732c3e18b
2sssg <lsg020300@naver.com>
2022-04-24 01:38:06 +0900

    Feat: StringValue to EnumValue Function

--- src/main/java/constant/ChannelTypeStr.java
+++ src/main/java/constant/ChannelTypeStr.java
@@ -1,5 +1,13 @@
 package constant;
 
 public enum ChannelTypeStr {
-    NAVER,NAVERMAP
+    NAVER,NAVERMAP;
+    public static ChannelTypeStr toChannelTypeStr(String channelTypeStr){
+        if(channelTypeStr.equals(NAVER.toString())){
+            return NAVER;
+        }else if(channelTypeStr.equals(NAVERMAP.toString())){
+            return NAVERMAP;
+        }
+        return NAVER;
+    }
 }
